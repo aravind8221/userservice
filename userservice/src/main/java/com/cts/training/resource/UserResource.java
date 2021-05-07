b7dc50d42ac9cf1c34724c528cbf4b33fe3ec274
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-07 13:23:42 +0530

    Update UserResource.java

--- userservice/src/main/java/com/cts/training/resource/UserResource.java
+++ userservice/src/main/java/com/cts/training/resource/UserResource.java
@@ -3,6 +3,8 @@ package com.cts.training.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.training.bean.User;
+import com.cts.training.exception.UserNotFoundException;
 import com.cts.training.repository.UserRepository;
 
 @RestController
@@ -28,14 +32,21 @@ public class UserResource {
 	public List<User> getAllUsers(){
 		return userRepository.findAll();
 	}
+	
 	@GetMapping("/users/{id}")
 	public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
 		Optional<User> user = userRepository.findById(id);
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("The user with id - "+id+ " does not exists");
+		}
 		return new ResponseEntity<>(user, HttpStatus.FOUND);
 	}
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable Long id) {
-		
+		Optional<User> user = userRepository.findById(id);
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("The user with id - "+id+ " does not exists");
+		}
 		userRepository.deleteById(id);
 	}
 	@GetMapping("/users/address/{address}")
@@ -44,8 +55,18 @@ public class UserResource {
 		return users;
 	}
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public User createUser(@Valid @RequestBody User user) {
 		User savedUser=userRepository.save(user);
 		return savedUser;
+	}
+	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<Object> updateUser(@Valid @RequestBody User user,@PathVariable Long id){
+		Optional<User> updatedUser = userRepository.findById(id);
+		if(!updatedUser.isPresent()) {
+			throw new UserNotFoundException("The user with id - "+id+ " does not exists");
+		}
+		userRepository.save(user);
+		return ResponseEntity.noContent().build();
 	}
 }
