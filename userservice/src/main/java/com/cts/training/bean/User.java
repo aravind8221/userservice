2f3dbc770a4b41b92b04312fece6ccdee1d42be4
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-05-07 13:21:54 +0530

    Update User.java

--- userservice/src/main/java/com/cts/training/bean/User.java
+++ userservice/src/main/java/com/cts/training/bean/User.java
@@ -3,6 +3,9 @@ package com.cts.training.bean;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 //@Table(name="user")
@@ -11,8 +14,12 @@ public class User {
 	@Id
 	//@Column(name="user_id")
 	private Long id;
+	@NotEmpty(message = "The Username field can not be Empty!")
+	@Size(min=8,max=50, message="The Username Should be minimum of 8 Charecters")
 	private String username;
+	@NotBlank(message="The Paassword field cannot be Empty")
 	private String password;
+	@NotEmpty(message="The address field cannot be Empty")
 	private String address;
 	private Long phone;
 
