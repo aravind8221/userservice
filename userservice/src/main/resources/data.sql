0ee74b530ffb10d7fe2ad5df5d4980e3a84fb6a5
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2022-07-21 14:29:43 +0530

    Delete data.sql

--- userservice/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user(id,username,password,address,phone) values(1001L,'mahesh','mahesh123','Banglore',6624773288L);
-insert into user(id,username,password,address,phone) values(1002L,'suresh','suresh123','Chennai',63468463437L);
-insert into user(id,username,password,address,phone) values(1003L,'nikhil','nikhil123','Mumbai',84379873987L);
-insert into user(id,username,password,address,phone) values(1004L,'aravind','aravind123','Hyderabad',662477388L);
-insert into user(id,username,password,address,phone) values(1005L,'arun','arun123','Kochi',5474773288L);
